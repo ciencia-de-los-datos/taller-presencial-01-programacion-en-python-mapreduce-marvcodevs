df7cc4145b03ea0cc8da8ecd321c6c279b97cea7
Marco Rodríguez <marodriguezvar@unal.edu.co>
2024-02-23 20:02:31 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -14,6 +14,7 @@
 #   ]
 #
 import glob
+import os.path
 import fileinput
 
 
@@ -48,9 +49,10 @@ def mapper(sequence):
     for _, text in sequence:
         words = text.split()
         for word in words:
-            new_sequence.append((word, 1))
+            word = word.replace(",", "")
+            word = word.replace(".", "")
+            new_sequence.append((word,1))
     return new_sequence
-
 
 #sequence = load_input('input')
 #sequence = mapper(sequence)
@@ -72,9 +74,9 @@ def shuffle_and_sort(sequence):
     sorted_sequence = sorted(sequence, key=lambda x: x[0])
     return sorted_sequence
 
-sequence = load_input('input')
-sequence = mapper(sequence)
-sequence = shuffle_and_sort(sequence)
+# sequence = load_input('input')
+# sequence = mapper(sequence)
+# sequence = shuffle_and_sort(sequence)
 #print(sequence)
 
 
@@ -101,19 +103,31 @@ def reducer(sequence):
     return new_sequence
 
 
-sequence = load_input('input')
-sequence = mapper(sequence)
-sequence = shuffle_and_sort(sequence)
-sequence = reducer(sequence)
-print(sequence)
+# sequence = load_input('input')
+# sequence = mapper(sequence)
+# sequence = shuffle_and_sort(sequence)
+# sequence = reducer(sequence)
+#print(sequence)
 
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
 #
-def create_ouptput_directory(output_directory):
-    pass
+def create_output_directory(output_directory):
+
+    if os.path.exists(output_directory):
+        raise FileExistsError(f"The directory '{output_directory}' already exists.")
+    os.makedirs(output_directory)
+
+
+
+# sequence = load_input('input')
+# sequence = mapper(sequence)
+# sequence = shuffle_and_sort(sequence)
+# sequence = reducer(sequence)
+# create_output_directory('output')
+#print(sequence)
 
 
 #
@@ -125,26 +139,39 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+
+    with open(output_directory + "/part-00000", "w") as file:
+        for key, value in sequence:
+            file.write(f'{key}\t{value}\n')
 
 
 #
 # La siguiente función crea un archivo llamado _SUCCESS en el directorio
 # entregado como parámetro.
 #
+
 def create_marker(output_directory):
-    pass
+    with open(output_directory + "/_SUCCESS", "w") as file:
+        file.write("")
+        
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
+
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input(input_directory)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_output_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
     job(
         "input",
         "output",
-    )'''
+    )
